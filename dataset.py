0974ff7bd28fcced0948a408ed5f6af22f8c1788
Ian Leefmans <iel230@nyu.edu>
2020-07-12 15:50:14 -0400

    edited dataset to work with origional image labels

--- dataset.py
+++ dataset.py
@@ -42,8 +42,6 @@ class train_dataset(torch.utils.data.Dataset):
                 on a sample
             channels (string): ['RGB'(default), 'CMYK'] color channels to convert
                 all images to
-
-        Note: Fig.0 = Fig.819 for labels and captions!!!!
         """
 
         self.root_dir = root_dir
@@ -56,10 +54,12 @@ class train_dataset(torch.utils.data.Dataset):
 
     def __getitem__(self, index):
         
-        if index<819:
+        if index==0:
+            img_path = os.path.join(self.root_dir, f'Fig.{index+819}.jpg')
+        elif index<819:
             img_path = os.path.join(self.root_dir, f'Fig.{index}.jpg')
             print(img_path)
-        else:
+        elif index<1670:
             missing_values, key_dict = missing_keys(self.root_dir)
             if index-806 in missing_values:
                 img_path = os.path.join(self.root_dir, f'img_{key_dict[index-806]}.jpg')
@@ -67,9 +67,17 @@ class train_dataset(torch.utils.data.Dataset):
             else:
                 img_path = os.path.join(self.root_dir, f'img_{index-806}.jpg')
                 print(img_path)
+        #--------------------------------------------------------------------------------
+        
+        #else:
+        #img_path = os.path.join(
+        
+        #--------------------------------------------------------------------------------
+
+
     
         image = Image.open(img_path)
-        #--------------------------------------------------------------------------------
+                                    
         if self.channels=='RGB':
             if image.mode=='CMYK':
                 image = image.convert('RGB')
@@ -79,7 +87,6 @@ class train_dataset(torch.utils.data.Dataset):
         else:
             print("Invalid Channel Type")
 
-        #---------------------------------------------------------------------------------
         
         if self.transform:
             image = self.transform(image)
