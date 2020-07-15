f06217253481219b48aa8e680943baabd1869772
Ian Leefmans <iel230@nyu.edu>
2020-07-15 18:28:14 -0400

    added function to make images uniform

--- dataset.py
+++ dataset.py
@@ -28,22 +28,55 @@ def path_dict(folder_path):
     return dic
 
 
+# Function to expand or reduce size of image keeping aspect ration intact
+def expand(image, largest_dimension, resample=0):
+    #resample options (see Pil.Image.resize for more documentation):
+    #0 :Nearest neighbors (default)
+    #PIL.Image.BILINEAR
+    #Image.BICUBIC
+    #Image.LANCZOS
+    if image.height>= image.width:
+        new_width = round((image.width/image.height)*largest_dimension)
+        new_height = largest_dimesion
+    else:
+        new_height = round((image.height/image.width)*largest_dimension)
+        new_width = largest_dimension
+    image = image.resize((new_width, new_height), resample=resample )
+    return image
+
+# Function to add padding to make pictures uniform size
+def uniform_size(x, height, width):
+    tup_val1 = round(((width-x.size()[2])/2)-.1)
+    tup_val2 = round(((width-x.size()[2])/2)+.1)
+    tup_val3 = round(((height-x.size()[1])/2)-.1)
+    tup_val4 = round(((height-x.size()[1])/2)+.1)
+    
+    tup = (tup_val1, tup_val2, tup_val3, tup_val4)
+    
+    pad = nn.ZeroPad2d(tup)
+    padded = pad(x)
+    return padded
+
+
 
 # Class to create dataset with annotation
 class train_dataset(torch.utils.data.Dataset):
-    def __init__(self, annotation_dir, train_dict, img_dir, transform=None, channel='RGB'):
+    def __init__(self, annotation_dir, train_dict, img_dir, transform=None, img_size=(600,600)):
         """
             Args:
-            img_dir (string): Train Directory with images
             
-            train_dict (dictionary): Dictionary containing folder and image matches for second part of 
+            annotation_dir (string): Directory too json containg training annotation
+            
+            train_dict (dictionary): Dictionary containing folder and image matches for second part of
                 train data
+            
+            img_dir (string): Train Directory with images
                 
             transform (callable, optional): Optional transform to be applied
-            on a sample
+                on a sample
             
-            channels (string): ['RGB'(default), 'CMYK'] color channels to convert
-                all images to
+            img_size (tuple): (height, width) Desired height and width for all images to conform to. Height
+                must equal width.
             """
         
         self.img_dir = img_dir
@@ -51,7 +84,8 @@ class train_dataset(torch.utils.data.Dataset):
         self.new_annotation = self.annotation.loc[self.annotation.Images!='img_233',:].reset_index()
         self.train_dict2 = train_dict
         self.transform = transform
-        self.channels = channel
+        #self.channels = channel
+        self.img_size = img_size
     
     def __len__(self):
         return len(self.new_annotation)
@@ -69,18 +103,27 @@ class train_dataset(torch.utils.data.Dataset):
     
         image = Image.open(img_path)
         
-        if self.channels=='RGB':
-            if image.mode=='CMYK':
-                image = image.convert('RGB')
-        elif self.channels=='CMYK':
-            if image.mode=='RGB':
-                image = image.convert('CMYK')
+        #if self.channels=='RGB':
+        if image.mode=='CMYK':
+            image = image.convert('RGB')
+        #elif self.channels=='CMYK':
+            #if image.mode=='RGB':
+                #image = image.convert('CMYK')
         else:
             print("Invalid Channel Type")
+
+        if img_size[0] == img_size[1]:
+            image = expand(image, img_size[0])
+        else:
+            raise Exception("Attribute Error: Image size must be square")
+        
 
 
         if self.transform:
             image = self.transform(image)
+        
+        image = uniform_size(image, img_size[0], img_size[1])
+        
 
 
 
