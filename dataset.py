ff68e1d5806386570081cd53f351ba7eb11e5de7
Ian Leefmans <iel230@nyu.edu>
2020-07-20 23:24:39 -0400

    moved image tranformation functions to class in preprocess script

--- dataset.py
+++ dataset.py
@@ -9,6 +9,8 @@ import torch
 from torch import nn
 import torchvision
 
+from preprocess import image_process
+
 
 
 
@@ -27,44 +29,6 @@ def path_dict(folder_path):
                 pass
 
     return dic
-
-
-# Function to expand or reduce size of image keeping aspect ration intact
-def expand(image, desired_dimensions, resample=0):
-    #desired_dimensions (tuple): (height, width)
-    #resample options (see Pil.Image.resize for more documentation):
-    #0 :Nearest neighbors (default)
-    #PIL.Image.BILINEAR
-    #Image.BICUBIC
-    #Image.LANCZOS
-    
-    new_width = round((image.width/image.height)*desired_dimensions[0])
-    new_height = round((image.height/image.width)*desired_dimensions[1])
-    
-    if new_width<= desired_dimensions[1]:
-        new_height = desired_dimensions[0]
-    elif new_height<= desired_dimensions[0]:
-        new_width = desired_dimensions[1]
-    else:
-        raise Exception("Error in expand() go back and check how images are resized")
-    
-    image = image.resize((new_width, new_height), resample=resample )
-    return image
-
-
-
-# Function to add padding to make pictures uniform size
-def uniform_size(x, height, width):
-    tup_val1 = round(((width-x.size()[2])/2)-.1)
-    tup_val2 = round(((width-x.size()[2])/2)+.1)
-    tup_val3 = round(((height-x.size()[1])/2)-.1)
-    tup_val4 = round(((height-x.size()[1])/2)+.1)
-    
-    tup = (tup_val1, tup_val2, tup_val3, tup_val4)
-    
-    pad = nn.ZeroPad2d(tup)
-    padded = pad(x)
-    return padded
 
 
 
@@ -93,8 +57,9 @@ class train_dataset(torch.utils.data.Dataset):
         self.new_annotation = self.annotation.loc[self.annotation.Images!='img_233',:].reset_index()
         self.train_dict2 = train_dict
         self.transform = transform
-        #self.channels = channel
         self.img_size = img_size
+        self.preprocess = image_process(self.img_size)
+
     
     def __len__(self):
         return len(self.new_annotation)
@@ -112,26 +77,19 @@ class train_dataset(torch.utils.data.Dataset):
     
         image = Image.open(img_path)
         
-        #if self.channels=='RGB':
         if image.mode != 'RGB':
             image = image.convert('RGB')
-        #elif self.channels=='CMYK':
-            #if image.mode=='RGB':
-                #image = image.convert('CMYK')
-        #else:
-            #print("Invalid Channel Type")
+        
 
-        image = expand(image, self.img_size)
+        image = self.preprocess.expand(image)
         
 
 
         if self.transform:
             image = self.transform(image)
         
-        image = uniform_size(image, self.img_size[0], self.img_size[1])
+        image = self.preprocess.uniform_size(image)
         
-
-
 
         question = self.new_annotation.Questions[index]
         answer = self.new_annotation.Answers[index]
