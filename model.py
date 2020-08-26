52ea3456ea3e1e5f122a662e390f842f182ea48b
Ian Leefmans <iel230@nyu.edu>
2020-08-25 23:31:46 -0400

    started primary model script

---
+++ model.py
@@ -0,0 +1,43 @@
+import numpy as np
+import torch
+import torch.nn as nn
+import torch.nn.functional as f
+import torchvision
+
+import os
+
+
+
+
+
+class Model(nn.Module):
+    
+    """
+        Primary VQA Model
+    """
+    def __init__(self, num_features):
+        super(Model, self).__init__()
+
+        self.
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
