194ba96512e63ac3760a277ff5e947bd922fac19
Ian Leefmans <iel230@nyu.edu>
2020-09-05 13:34:56 -0400

    Create folder for unit test scripts

---
+++ unit_tests/test_pretrain_VAE.py
@@ -0,0 +1,36 @@
+import unittest
+import torch
+from pretrain_VAE import VAE
+
+
+class test_image_process(unittest.TestCase):
+    
+    def setUp(self):
+        self.x = torch.rand(4,3,491,600)
+    
+    
+    
+    
+    
+    
+    
+    def tearDown(self):
+        pass
+
+
+    def test_VAE_output_size(self):
+        self.assertEqual(VAE(16).forward(self.x)[0].size(), torch.Size([4,3,491,600]))
+    
+
+
+
+
+
+
+if __name__ == '__main__':
+    unittest.main()
+
+
+
+
+
